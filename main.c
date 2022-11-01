aad32a51494895dfad072dfef9525a9b11eee0a6
hoin2 <jingyusin20@sookmyung.ac.kr>
2022-11-01 16:40:37 +0900

    04

--- main.c
+++ main.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 5
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#define SIZE 4
+
+void square_array(int a[],int size);
+void print_array(int a[],int size);
 
 int main() {
-	int i;
-	int a[SIZE]={1,2,3,4,5};
-	int b[SIZE]={1,2,3,4,5};
-	int flag_same=1;
+	int list[SIZE]={1,2,3,4};
 	
-	for(i=0;i<SIZE;i++){
-		if(a[i]!=b[i]){
-			printf("a[%d],b[%d] 배열은 서로 다른 값을 가집니다.\n",i,i);
-			flag_same=0;
-			}
-	}
+	print_array(list,SIZE);
+	square_array(list,SIZE);
+	print_array(list,SIZE);
 		
 	return 0;
+}
+
+void square_array(int a[],int size){
+	int i;
+	
+	for(i=0;i<size;i++)
+		a[i]=a[i]*a[i];
+}
+
+void print_array(int a[],int size){
+	int i;
+	
+	for(i=0;i<size;i++)
+		printf("%3d",a[i]);
+	
+	printf("\n");
 }
