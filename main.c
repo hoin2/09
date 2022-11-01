e71ed6bbe091ccead11ad1f172aaad0cff50565a
hoin2 <jingyusin20@sookmyung.ac.kr>
2022-11-01 16:27:00 +0900

    02-2

--- main.c
+++ main.c
@@ -4,15 +4,19 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	int i;
+	int i,average;
+	int sum;
 	int grade[SIZE];
-	printf("5명의 점수를 입력하세요.\n");
 	
-	for(i=0;i<SIZE;i++)
+	sum=0;
+	for(i=0;i<SIZE;i++){
+		printf("학생 성적을 입력하세요:");
 		scanf("%d",&grade[i]);
+		sum+=grade[i];
+	}
 	
-	for(i=0;i<SIZE;i++)
-		printf("grade[%d]=%d\n",i,grade[i]);
+	average=sum/SIZE;
+	printf("성적 평균:%d\n",average);
 		
 	return 0;
 }
